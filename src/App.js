ba8944ede4e9be15d736dd9cca4efb4bf7fce08e
Jenseneducation-Lukas-Tucker <54941820+Jenseneducation-Lukas-Tucker@users.noreply.github.com>
2020-10-29 11:08:31 +0100

    added saloonlist as state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useState, useEffect} from "react";
+import React, { Component } from "react";
+import './App.css';
+import FilterList from './FilterList';
+import Saloon from './Saloon.js'
+import SaloonList from './SaloonList.js'
 
-function App() {
-  const [data, SetData] = useState([])
-  const [q, setQ] = useState("")
-  return (
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Saloons: [
+        {name:'Sax & Fön', price:'320 kr', time:'30 min', rating: 4, location:'Rådmansgatan 46'},
+        {name:'Håriszont', price:'320 kr', time:'30 min', rating: 4, location:'Rådmansgatan 46'},
+        {name:'Hårhuset', price:'320 kr', time:'30 min', rating: 4, location:'Rådmansgatan 46'},
+        {name:'Hair & Nu', price:'320 kr', time:'30 min', rating: 4, location:'Rådmansgatan 46'},
+        {name:'Hårley Davidson', price:'320 kr', time:'30 min', rating: 4, location:'Rådmansgatan 46'}
+      ]
+    }
+  }
+  
+  render() {
+    return (
     <div className="App">
-      <div>filter goes here</div>
-      <div>datatable goes here</div>
+      <h1>Saloon List</h1>
     </div>
   );
 }
